37d46eb6c83ff3d7dce37f7a7bd0415cfbf8ca7c
Sérgio Henrique Menta Garcia <garciasergiodm@gmail.com>
2022-12-07 23:52:02 -0300

    Adiciona função para atribuir classificação a partir do IMC

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,25 +11,41 @@ export class HomePage {
   weight: number = 0
   height: number = 0
   imc: number = 0
+  classificao: string = ""
 
-  constructor(private toastCtrl: ToastController) {}
+  constructor(private toastCtrl: ToastController) { }
 
   onCalculate() {
     if (this.height <= 0 || this.weight <= 0) {
       return
     }
-    
+
     this.imc = this.weight / (this.height * this.height)
+    this.onSetClassificacao()
     this.showIMC()
+  }
+
+  onSetClassificacao() {
+    if (this.imc < 18.5) {
+      this.classificao = "Magreza"
+    } else if (this.imc >= 18.5 && this.imc < 25.0) {
+      this.classificao = "Normal"
+    } else if (this.imc >= 25.0 && this.imc < 30.0) {
+      this.classificao = "Sobrepeso"
+    } else if (this.imc >= 30 && this.imc < 40) {
+      this.classificao = "Obesidade"
+    } else {
+      this.classificao = "Obesidade Grave"
+    }
   }
 
   async showIMC() {
     const toast = await this.toastCtrl.create({
-      message: `IMC = ${this.imc.toFixed(2)}`,
+      message: `IMC = ${this.imc.toFixed(2)} - Classificação = ${this.classificao}`,
       duration: 3000,
       color: 'secondary'
     })
-    
+
     toast.present()
 
   }
